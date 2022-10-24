ddb0eed7ce107f9d873f38747f6ca134635f40b3
Carlos Tovar <carlos.tovar@eam.edu.co>
2022-10-24 07:53:00 -0500

    Config-data-y-service
    Creación config-data, config-service y centralización de archivos de configuración.

---
+++ config-service/src/test/java/co/edu/eam/negocio/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eam.negocio.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
