7267829c782bbb5cb59a2b9355a76b8ecd79382c
Carlos Tovar <carlos.tovar@eam.edu.co>
2022-10-31 08:05:23 -0500

    service-registry
    Creación del servicio de registro Eureka Server.

---
+++ service-registry/src/test/java/co/edu/eam/negocio/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eam.negocio.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
